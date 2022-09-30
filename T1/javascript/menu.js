c07651079b7454408349fe5086cc0e13146c60b8
ThiagoNicacio <tgomesnicacio@gmail.com>
2022-09-30 13:16:04 -0300

    finalizando a funcionalidade de menu

---
+++ T1/javascript/menu.js
@@ -0,0 +1,24 @@
+window.onload = load;
+
+function load(){
+    var exit = document.getElementById("exit");
+    var start = document.getElementById("btnMenu")
+
+    exit.addEventListener('click', (event) => {
+        //clear session storage e limpa o histórico de navegação
+        console.log('exit click')
+        sessionStorage.clear()
+        window.location.replace("index.html")
+    });
+
+    start.addEventListener('click', (event) =>{
+        console.log('Start Click')
+        var difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+        console.log(difficulty)
+
+        sessionStorage.setItem('difficulty', difficulty)
+    })
+
+    var name = document.getElementById("email");
+    name.textContent = sessionStorage.getItem("email")
+}
