901a28735c00354dbae57618a0b288fe715c05a2
Thiago Nicacio <tgomesnicacio@gmail.com>
2022-10-15 14:50:48 -0300

    função para gerar uma lista randomica com os itens a serem exibidos no jogo

--- T1/javascript/game.js
+++ T1/javascript/game.js
@@ -2,6 +2,23 @@ var startButton = undefined
 var stopButton = undefined
 var controls = undefined
 
+let cards; 
+
+const items = [
+    { name: "batman", image: "src/batman.jpg" },
+    { name: "capitao", image: "src/capitao-america.png" },
+    { name: "doutor", image: "src/doutor-estranho.jpg" },
+    { name: "feiticeira", image: "src/feiticeira-escarlate.jpg" },
+    { name: "gamora", image: "src/gamora.jpg" },
+    { name: "groot", image: "src/groot.jpg" },
+    { name: "homem-aranha", image: "src/homem-aranha.jpg" },
+    { name: "homem-ferro", image: "src/homem-de-ferro.png" },
+    { name: "hulk", image: "src/hulk.jpg" },
+    { name: "mulher-maravilha", image: "src/mulher-maravilha.jpg" },
+    { name: "pantera", image: "src/pantera-negra.jpg" },
+    { name: "thor", image: "src/thor.png" },
+  ];
+
 window.onload = function (){
     var exitGameButton = document.getElementById("exitGame");
     startButton = document.getElementById("start");
@@ -38,5 +55,22 @@ window.onload = function (){
 
 
 function startGame(){
-    console.log('game start')
+    console.log('game start');
+
+    let cardValues = generateRandomArrayCards();
+    console.log(cardValues);
 }
+
+const generateRandomArrayCards = (size = 4) => {
+    let tArray = [...items];
+    let cardValues = [];
+    size = (size * size) / 2;
+    for (let i = 0; i < size; i++) {
+      const randomIndex = Math.floor(Math.random() * tArray.length);
+      cardValues.push(tArray[randomIndex]);
+      tArray.splice(randomIndex, 1);
+    }
+
+    return cardValues;
+};
+
