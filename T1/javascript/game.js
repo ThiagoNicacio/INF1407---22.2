37f9ec377a2a8cd754fbdf2f8e9c7b703c19bb1f
Thiago Nicacio <tgomesnicacio@gmail.com>
2022-10-14 23:34:00 -0300

    limpando código e adicionando imagens do quebra cabeças

--- T1/javascript/game.js
+++ T1/javascript/game.js
@@ -1,8 +1,3 @@
-var canvas;
-var ctx;
-
-var Width = 0, Height = 0,transX = 0, transY = 0;
-
 window.onload = function (){
     var exit = document.getElementById("exit");
 
@@ -13,47 +8,6 @@ window.onload = function (){
         window.location.replace("index.html")
     });
 
-    // addEventListener('mousemove', (event) => {
-    //     var x = event.clientX;
+    
 
-    //     console.log(x, transX)
-    // })
-
-    plot();
-}
-
-
-function plot(){
-
-    canvas = document.getElementById("plotCanvas");
-
-    if (canvas.getContext) {
-	    ctx = canvas.getContext("2d");
-
-        canvas.addEventListener('mousemove', (event) => {
-            var rect = canvas.getBoundingClientRect();
-            var x = event.clientX - transX;
-            var y = event.clientY - transY;
-            console.log("x: " + x + " y: " + y); 
-        })
-        
-        Width = canvas.width;
-        Height = canvas.height;
-
-        transX = Math.abs(Width * 0.5),
-        transY = Math.abs(Height * 0.5);
-
-        ctx.clearRect(0,0,Width,Height);
-        ctx.translate(transX, transY);
-        
-        console.log(transX, transY)
-
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(2, -10);
-        ctx.lineTo(5, 0)
-        
-        ctx.fillStyle = 'red';
-        ctx.stroke()
-      }
 }
