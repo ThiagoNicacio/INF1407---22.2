d4de6f40ff923878fc56fec9d709884032ee5df9
Thiago Nicacio <tgomesnicacio@gmail.com>
2022-10-15 21:02:11 -0300

    alterando as imagens do jogo, iniciando a exibição dos cards em uma matrix 4x4

--- T1/javascript/game.js
+++ T1/javascript/game.js
@@ -1,21 +1,22 @@
 var startButton = undefined
 var stopButton = undefined
 var controls = undefined
+var gameContainer = undefined
 
 let cards; 
 
 const items = [
-    { name: "batman", image: "src/batman.jpg" },
+    { name: "batman", image: "src/batman.png" },
     { name: "capitao", image: "src/capitao-america.png" },
-    { name: "doutor", image: "src/doutor-estranho.jpg" },
-    { name: "feiticeira", image: "src/feiticeira-escarlate.jpg" },
-    { name: "gamora", image: "src/gamora.jpg" },
-    { name: "groot", image: "src/groot.jpg" },
-    { name: "homem-aranha", image: "src/homem-aranha.jpg" },
+    { name: "doutor", image: "src/doutor-estranho.png" },
+    { name: "deadpool", image: "src/deadpool.png" },
+    { name: "rocket", image: "src/rocket.png" },
+    { name: "groot", image: "src/groot.png" },
+    { name: "homem-aranha", image: "src/homem-aranha.png" },
     { name: "homem-ferro", image: "src/homem-de-ferro.png" },
-    { name: "hulk", image: "src/hulk.jpg" },
-    { name: "mulher-maravilha", image: "src/mulher-maravilha.jpg" },
-    { name: "pantera", image: "src/pantera-negra.jpg" },
+    { name: "hulk", image: "src/hulk.png" },
+    { name: "mulher-maravilha", image: "src/mulher-maravilha.png" },
+    { name: "pantera", image: "src/pantera-negra.png" },
     { name: "thor", image: "src/thor.png" },
   ];
 
@@ -24,6 +25,7 @@ window.onload = function (){
     startButton = document.getElementById("start");
     stopButton = document.getElementById("stop");
     controls = document.getElementById("controls");
+    gameContainer = document.querySelector(".game-container");
 
     exitGameButton.addEventListener('click', () => {
         console.log('exit click');
@@ -53,12 +55,12 @@ window.onload = function (){
     })
 }
 
-
 function startGame(){
     console.log('game start');
 
     let cardValues = generateRandomArrayCards();
     console.log(cardValues);
+    matrixGenerator(cardValues);
 }
 
 const generateRandomArrayCards = (size = 4) => {
@@ -74,3 +76,39 @@ const generateRandomArrayCards = (size = 4) => {
     return cardValues;
 };
 
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = "";
+    cardValues = [...cardValues, ...cardValues];
+    //simple shuffle
+    cardValues.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < size * size; i++) {
+      
+        var cardBefore = document.createElement('div');
+        cardBefore.classList.add('card-before');
+        cardBefore.appendChild(document.createTextNode('X'))
+        //console.log('cardBefore   ', cardBefore);
+
+        var img = document.createElement('img');
+        img.src = cardValues[i].image;
+        img.classList.add('image');
+        //console.log('img   ', img);
+
+        var cardAfter = document.createElement('div');
+        cardAfter.classList.add('card-after');
+        cardAfter.appendChild(img);
+        //console.log('cardAfter   ', cardAfter);
+
+        var cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container');
+        cardContainer.setAttribute('data-card-value',cardValues[i].name);
+        cardContainer.appendChild(cardBefore);
+        cardContainer.appendChild(cardAfter);
+        //console.log('cardContainer   ', cardContainer);
+
+        gameContainer.appendChild(cardContainer);
+        
+    }
+    
+    gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+
+}
